3b6d05bcd7cdd2a72705898438ff63498eefc438
Glennreble <141339442+Glennreble@users.noreply.github.com>
2023-09-01 20:00:09 +0800

    Update

--- js/illions.js
+++ js/illions.js
@@ -3,7 +3,8 @@ function getSIprefixes(i) {
         "yocto", "zepto", "atto", "femto", "pico",
         "nano", "micro", "milli", "", "kilo",
         "mega", "giga", "tera", "peta", "exa",
-        "zetta", "yotta"
+        "zetta", "yotta", "ronna", "quetta",
+        "henda", "doka", "alpha", "europa"
     ];
 
     return SIprefixes[i];
